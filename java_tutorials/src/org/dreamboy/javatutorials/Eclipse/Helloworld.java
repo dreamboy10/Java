fae5e24bbbc4d72405fd245c11f3c118398d9821
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2021-11-05 02:41:48 +0900

    2021.11.05

---
+++ java_tutorials/src/org/dreamboy/javatutorials/Eclipse/Helloworld.java
@@ -0,0 +1,10 @@
+package org.dreamboy.javatutorials.Eclipse;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("world Hello");
+	}
+
+}
---
+++ java_tutorials/src/org/dreamboy/javatutorials/NumberString/String.java
@@ -0,0 +1,20 @@
+package org.dreamboy.javatutorials.NumberString;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		System.out.println('생');
+		System.out.println("생활코딩");
+//		System.out.println('생활코딩');
+		System.out.println("생");
+		System.out.println("생활코딩"+"입니다");
+		System.out.println("1"+"1");
+//		System.out.println
+//		("egoing said "Welcome programming world"");
+		System.out.println
+		("egoing said \"Welcome programming world\"");
+		System.out.println
+		("egoing said\n \"Welcome programming world\"");
+	}
+
+}
