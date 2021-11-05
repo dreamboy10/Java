ab2acb9988c174993304b1a333500e7ace378bf0
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2021-11-06 00:17:31 +0900

    2021.11.05

---
+++ java_tutorials/src/org/dreamboy/javatutorials/Variable/StringDemo.java
@@ -0,0 +1,12 @@
+package org.dreamboy.javatutorials.Variable;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String a, b;
+		a = "coding";
+		b = "everybody";
+		System.out.println(a+b);
+	}
+
+}
---
+++ java_tutorials/src/org/dreamboy/javatutorials/Variable/WhyDemo.java
@@ -0,0 +1,20 @@
+package org.dreamboy.javatutorials.Variable;
+
+public class WhyDemo {
+
+	public static void main(String[] args) {
+//		System.out.println(100 + 10);
+//		System.out.println((100 + 10) / 10);
+//		System.out.println(((100 + 10) / 10) - 10);
+//		System.out.println
+//		((((100 + 10) / 10) - 10) * 10);
+		
+		int a = 100;
+		System.out.println(a + 10);
+		System.out.println((a+ 10) / 10);
+		System.out.println(((a + 10) / 10) - 10);
+		System.out.println
+		((((a + 10) / 10) - 10) * 10);
+	}
+
+}
