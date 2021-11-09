edd70df5549fb5fca3503adabfbc63432a93b66d
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2021-11-10 01:58:54 +0900

    2021.11.10

---
+++ java_tutorials/src/org/dreamboy/javatutorials/Constant/ConstantDemo.java
@@ -0,0 +1,11 @@
+package org.dreamboy.javatutorials.Constant;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		int a = 1;
+		System.out.println(a);
+//		1=3;
+	}
+
+}
---
+++ java_tutorials/src/org/dreamboy/javatutorials/Constant/ConstantDemo2.java
@@ -0,0 +1,18 @@
+package org.dreamboy.javatutorials.Constant;
+
+public class ConstantDemo2 {
+
+	public static void main(String[] args) {
+		double a = 2.2;
+		System.out.println(a);
+		float b = 2.2F;
+		System.out.println(b);
+		long c = 2147483648L;
+		System.out.println(c);
+		byte d = 100;
+		System.out.println(d);
+		short e = 200;
+		System.out.println(e);
+	}
+
+}
