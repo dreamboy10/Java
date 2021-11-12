f5c558b2c0fbc2c7fb94b05d7f2ed55e79e8d9a8
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2021-11-13 01:08:30 +0900

    2021.11.13

---
+++ java_tutorials/src/org/dreamboy/javatutorials/Loop/WhileDemo.java
@@ -0,0 +1,11 @@
+package org.dreamboy.javatutorials.Loop;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		while (true) {
+            System.out.println("Coding Everybody");
+        }
+	}
+
+}
