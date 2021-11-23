ffe4dabcd0f3a40e077661153980e8df409b041f
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2021-11-24 03:48:53 +0900

    2021.11.24

---
+++ java_tutorials/src/org/dreamboy/javatutorials/Input_Output/ScannerDemo.java
@@ -0,0 +1,14 @@
+package org.dreamboy.javatutorials.Input_Output;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i*1000);
+        sc.close();
+	}
+
+}
