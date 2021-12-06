7a952279ebcc34b4cbfc753fdcad621f84afdfc6
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2021-12-07 02:24:34 +0900

    2021.12.07

---
+++ java_tutorials/src/org/dreamboy/javatutorials/Inheritance/ConstructorDemo.java
@@ -0,0 +1,11 @@
+package org.dreamboy.javatutorials.Inheritance;
+
+public class ConstructorDemo {
+	public ConstructorDemo(){}
+    public ConstructorDemo(int param1) {}
+
+	public static void main(String[] args) {
+		ConstructorDemo  c = new ConstructorDemo();
+	}
+
+}
