599f529522363b66a882a2d269c0659275daf9f2
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2021-12-31 01:46:52 +0900

    2021.12.31

--- java_tutorials/src/org/dreamboy/javatutorials/Collection/SetDemo.java
+++ java_tutorials/src/org/dreamboy/javatutorials/Collection/SetDemo.java
@@ -1,27 +1,17 @@
 package org.dreamboy.javatutorials.Collection;
 
+import java.util.Collection;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
 public class SetDemo {
 
 	public static void main(String[] args) {
-		HashSet<Integer> A = new HashSet<Integer>();
+		Collection<Integer> A = new HashSet<Integer>();
         A.add(1);
         A.add(2);
         A.add(3);
-         
-        HashSet<Integer> B = new HashSet<Integer>();
-        B.add(3);
-        B.add(4);
-        B.add(5);
-         
-        HashSet<Integer> C = new HashSet<Integer>();
-        C.add(1);
-        C.add(2);
-         
-        System.out.println(A.containsAll(B));
-        System.out.println(A.containsAll(C));
          
         //A.addAll(B);
         //A.retainAll(B);
