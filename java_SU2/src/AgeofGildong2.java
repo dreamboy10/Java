5a99aa85c317d99ad6389311ab22bd3a49dc1e8a
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-03-06 01:58:15 +0900

    2022.03.06

---
+++ java_SU2/src/AgeofGildong2.java
@@ -0,0 +1,9 @@
+public class AgeofGildong2 {
+  public static void main(String[] args) {
+    int ageOfGildong = 9;
+    System.out.println(ageOfGildong);
+    System.out.println(ageOfGildong + 10);
+    System.out.println(ageOfGildong + 20);
+    System.out.println(ageOfGildong + 30);
+  }
+}
