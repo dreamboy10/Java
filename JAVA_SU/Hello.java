ba8a372717cfecb41df96b7e289f97ed6c422c8a
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-03-17 02:33:38 +0900

    2022.03.17

--- JAVA_SU/Hello.java
+++ JAVA_SU/Hello.java
@@ -1,5 +1,20 @@
+// 작성자: 이한정
+
+/*
+ * 작성일 : 2022.03.17.
+ * 목  적 : 첫 자바 프로그램 개발 방법을 보여주기 위해서
+*/
+
 public class Hello {
+  // 메인 메소드
   public static void main(String[] args) {
+    /* 
+      콘솔 출력 내용
+      Hello 내용을 출력
+    */
     System.out.println("Hello, welcome to the java world!");
+
+    /* System.out.println("Hello2");
+    System.out.println("Hello3"); */
   }
 }
