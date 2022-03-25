dd734dc0cb3938cab84586fdc76f5aed23e7f064
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-03-26 01:11:31 +0900

    2022.03.26

--- JAVA_SU/chap02/src/sec03/exam03_accuracy/FromIntTODouble.java
+++ JAVA_SU/chap02/src/sec03/exam03_accuracy/FromIntTODouble.java
@@ -1,12 +1,12 @@
-package sec03.exam03.accuracy;
+package sec03.exam03_accuracy;
 
-public class FromIntTOFloat {
+public class FromIntTODouble {
 
 	public static void main(String[] args) {
 		int num1 = 123456780;
 		int num2 = 123456780;
 		
-		float num3 = num2;
+		double num3 = num2;
 		num2 = (int) num3;
 		
 		int result = num1 - num2;
--- JAVA_SU/chap02/src/sec03/exam03_accuracy/FromIntTOFloat.java
+++ JAVA_SU/chap02/src/sec03/exam03_accuracy/FromIntTOFloat.java
@@ -1,12 +1,12 @@
-package sec03.exam03.accuracy;
+package sec03.exam03_accuracy;
 
-public class FromIntTODouble {
+public class FromIntTOFloat {
 
 	public static void main(String[] args) {
 		int num1 = 123456780;
 		int num2 = 123456780;
 		
-		double num3 = num2;
+		float num3 = num2;
 		num2 = (int) num3;
 		
 		int result = num1 - num2;
