a13a4d9ae5c839dd061341ab180c862262340c3c
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-04-29 01:44:06 +0900

    2022.04.29

---
+++ JAVA_SU/chap05/src/chap05/exam01_datatype/DataTypeExample.java
@@ -0,0 +1,20 @@
+package chap05.exam01_datatype;
+
+public class DataTypeExample {
+
+	public static void main(String[] args) {
+		int age = 25;
+		double price = 100.5;
+		
+		System.out.println(age);
+		System.out.println(price);
+		
+		String name = "이한정";
+		String hobby = "독서";
+		
+		System.out.println(name);
+		System.out.println(hobby);
+
+	}
+
+}
