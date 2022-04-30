671d3e81404fd5b39d93636bbdb5e6fe3a401e6d
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-01 01:42:08 +0900

    2022.05.01

---
+++ JAVA_SU/chap05/src/chap05/exam03_rerfernce/RerfernceExample.java
@@ -0,0 +1,28 @@
+package chap05.exam03_rerfernce;
+
+public class RerfernceExample {
+
+	public static void main(String[] args) {
+		int v1 = 10;
+		int v2 = 100;
+		int v3 = 100;
+		
+		System.out.println(v1 == v2);
+		System.out.println(v1 != v2);
+		
+		System.out.println(v2 == v3);
+		System.out.println(v2 != v3);
+		
+		var refVar1 = 100;
+		var refVar2 = 200;
+		var refVar3 = 200;
+		
+		System.out.println(refVar1 == refVar2);
+		System.out.println(refVar1 != refVar2);
+		
+		System.out.println(refVar2 == refVar3);
+		System.out.println(refVar2 != refVar3);
+
+	}
+
+}
