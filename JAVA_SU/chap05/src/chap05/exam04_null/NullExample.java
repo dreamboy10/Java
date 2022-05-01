d8167d89a60a6c2e9c7144225139c0f5f11d27d0
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-02 02:21:59 +0900

    2022.05.02

---
+++ JAVA_SU/chap05/src/chap05/exam04_null/NullExample.java
@@ -0,0 +1,18 @@
+package chap05.exam04_null;
+
+public class NullExample {
+
+	public static void main(String[] args) {
+		Object refVar1 = 20;
+		Object refVar2 = null;
+		
+		System.out.println(refVar1 == null);
+		System.out.println(refVar1 != null);
+		
+		System.out.println(refVar2 == null);
+		System.out.println(refVar2 != null);
+		
+
+	}
+
+}
---
+++ JAVA_SU/chap05/src/chap05/exam04_null/NullPointerExceptionExample.java
@@ -0,0 +1,18 @@
+package chap05.exam04_null;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		int[] intArray = null;
+		intArray[0] = 10;
+		
+		System.out.println(intArray);
+		
+		String str = null;
+		System.out.println
+		("ÃÑ ¹®ÀÚ¼ö : " + str.length());
+		
+
+	}
+
+}
