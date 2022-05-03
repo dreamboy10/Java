01dabf4319d268cf3dde862ee092c60604d0924b
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-04 01:58:12 +0900

    2022.05.04

---
+++ JAVA_SU/chap05/src/sec05/exam06_array/ArrayExample.java
@@ -0,0 +1,107 @@
+package sec05.exam06_array;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		int score1 = 83;
+		int score2 = 90;
+		int score3 = 100;
+		int score4 = 65;
+		int score5 = 9;
+		int score6 = 10;
+		int score7 = 1;
+		int score8 = 90;
+		int score9 = 63;
+		int score10 = 0;
+		int score11 = 43;
+		int score12 = 44;
+		int score13 = 50;
+		int score14 = 95;
+		int score15 = 92;
+		int score16 = 30;
+		int score17 = 35;
+		int score18 = 99;
+		int score19 = 23;
+		int score20 = 0;
+		int score21 = 43;
+		int score22 = 44;
+		int score23 = 50;
+		int score24 = 95;
+		int score25 = 92;
+		int score26 = 30;
+		int score27 = 35;
+		int score28 = 99;
+		int score29 = 23;
+		int score30 = 0;
+		
+		System.out.println(score1);
+		System.out.println(score2);
+		System.out.println(score3);
+		System.out.println(score4);
+		System.out.println(score5);
+		System.out.println(score6);
+		System.out.println(score7);
+		System.out.println(score8);
+		System.out.println(score9);
+		System.out.println(score10);
+		System.out.println(score11);
+		System.out.println(score12);
+		System.out.println(score13);
+		System.out.println(score14);
+		System.out.println(score15);
+		System.out.println(score16);
+		System.out.println(score17);
+		System.out.println(score18);
+		System.out.println(score19);
+		System.out.println(score20);
+		System.out.println(score21);
+		System.out.println(score22);
+		System.out.println(score23);
+		System.out.println(score24);
+		System.out.println(score25);
+		System.out.println(score26);
+		System.out.println(score27);
+		System.out.println(score28);
+		System.out.println(score29);
+		System.out.println(score30);
+		
+		int sum = score1;
+		sum += score2;
+		sum += score3;
+		sum += score4;
+		sum += score5;
+		sum += score6;
+		sum += score7;
+		sum += score8;
+		sum += score9;
+		sum += score10;
+		sum += score11;
+		sum += score12;
+		sum += score13;
+		sum += score14;
+		sum += score15;
+		sum += score16;
+		sum += score17;
+		sum += score18;
+		sum += score19;
+		sum += score20;
+		sum += score21;
+		sum += score22;
+		sum += score23;
+		sum += score24;
+		sum += score25;
+		sum += score26;
+		sum += score27;
+		sum += score28;
+		sum += score29;
+		sum += score30;
+		
+		int avg = sum / 30;
+		
+		System.out.println(avg);
+		
+	}
+}
+
+
+
---
+++ JAVA_SU/chap05/src/sec05/exam06_arraydec/ArrayDecExample.java
@@ -0,0 +1,14 @@
+package sec05.exam06_arraydec;
+
+public class ArrayDecExample {
+
+	public static void main(String[] args) {
+		/*
+		int[] intArray;
+		double[] doubleArray;
+		String[] strArray;
+		*/
+
+	}
+
+}
