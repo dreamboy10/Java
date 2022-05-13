026340867127ae76d5f15c9f3133a1d5d10da336
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-14 00:36:45 +0900

    2022.05.14

---
+++ JAVA_SU/chap05/src/sec05/exam11_arraycopy/ArrayCopyExample.java
@@ -0,0 +1,22 @@
+package sec05.exam11_arraycopy;
+
+public class ArrayCopyExample {
+
+	public static void main(String[] args) {
+		String[] oldStrArray = 
+		{"java", "array", "copy"};
+		String[] newStrArray = new String[5];
+		
+		System.arraycopy
+		(oldStrArray, 0, newStrArray, 
+		0, oldStrArray.length);
+		
+		for
+		(int i = 0; i < newStrArray.length; 
+		i++) {
+			System.out.print
+			(newStrArray[i] + " , ");
+		}
+	}
+
+}
