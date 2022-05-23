828f4c330db2c8b77f5f112d8cc4170a894a1bfc
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-24 01:19:10 +0900

    2022.05.24

---
+++ JAVA_SU/chap06/src/sec06/exam06_field/FieldExample.java
@@ -0,0 +1,41 @@
+package sec06.exam06_field;
+
+public class FieldExample {
+
+	public static void main(String[] args) {
+		String company;
+		String model;
+		String color;
+		int maxSpeed;
+		
+		int speed = 400;
+		double rpm = 1.25;
+		
+		String Body = "둥근사각형";
+		String Engine = "하이브리드";
+		String Tire = "금호타이어";
+		
+		company = "쉐보레";
+		model = "말리부";
+		color = "노란색";
+		maxSpeed = 400;
+		
+		System.out.println(company);
+		System.out.println(model);
+		System.out.println(color);
+		System.out.println(maxSpeed);
+		
+		System.out.println();
+		
+		System.out.println(speed);
+		System.out.println(rpm);
+		
+		System.out.println();
+		
+		System.out.println(Body);
+		System.out.println(Engine);
+		System.out.println(Tire);
+
+	}
+
+}
