e9ce17a735cc5ba30c614fa5d1dbb25ab2e597dd
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-03 01:38:04 +0900

    2022.06.03

---
+++ JAVA_SU/chap06/src/sec06/exam09_call/Car.java
@@ -0,0 +1,30 @@
+package sec06.exam09_call;
+
+public class Car {
+	// field
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
+		
+	// constructor
+	Car() {
+			
+	}
+		
+	Car(String model) {
+		this(model, "은색", 250);
+	}
+		
+	Car(String model, String color) {
+		this(model, color, 250);
+	}
+		
+	Car
+	(String model, String color, 
+	int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+}
