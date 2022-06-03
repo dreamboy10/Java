97e5ca169635df28787b125210ef8b776606c1b9
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-04 01:46:03 +0900

    2022.06.04

---
+++ JAVA_SU/chap06/src/sec06/exam10_method/CalculatorExample.java
@@ -0,0 +1,23 @@
+package sec06.exam10_method;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+		myCalc.powerOn();
+		
+		int result1 = myCalc.plus(10, 20);
+		System.out.println
+		("result1 : " + result1);
+		
+		byte x = 10;
+		byte y = 4;
+		double result2 = myCalc.divide(x, y);
+		System.out.println
+		("result2 : " + result2);
+		
+		myCalc.powerOff();
+
+	}
+
+}
---
+++ JAVA_SU/chap06/src/sec06/exam10_method/Computer.java
@@ -0,0 +1,20 @@
+package sec06.exam10_method;
+
+public class Computer {
+	int sum1(int[] values) {
+		int sum = 0;
+		for(int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+	
+	int sum2(int ... values) {
+		int sum = 0;
+		for(int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+	
+}
