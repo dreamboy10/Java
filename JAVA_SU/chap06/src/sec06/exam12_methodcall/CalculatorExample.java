14e4c4e6955f66167b9849977e9d474185b53ff9
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-14 02:02:03 +0900

    2022.06.14

---
+++ JAVA_SU/chap06/src/sec06/exam12_methodcall/CalculatorExample.java
@@ -0,0 +1,11 @@
+package sec06.exam12_methodcall;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+		myCalc.execute();
+
+	}
+
+}
