554dc0869b877ead408b3b5b634913efcb1be592
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-17 01:44:41 +0900

    2022.06.17

---
+++ JAVA_SU/chap06/src/sec06/exam14_instance_member/Car.java
@@ -0,0 +1,27 @@
+package sec06.exam14_instance_member;
+
+public class Car {
+	// field
+	String model;
+	int speed;
+	
+	// constructor
+	Car(String model) {
+		this.model = model;
+	}
+	
+	// method
+	void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	void run() {
+		for(int i = 10; i <= 50; i += 10) {
+			this.setSpeed(i);
+			System.out.println
+			(this.model + "가 달립니다."
+					+ "(시속 : " + 
+					this.speed + "km/h)");
+		}
+	}
+}
