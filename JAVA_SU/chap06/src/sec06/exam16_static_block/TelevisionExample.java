8e764e7beb3a2dd3851d618f5ec77fe21d1a01a4
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-22 02:14:28 +0900

    2022.06.22

---
+++ JAVA_SU/chap06/src/sec06/exam16_static_block/TelevisionExample.java
@@ -0,0 +1,12 @@
+package sec06.exam16_static_block;
+
+public class TelevisionExample {
+
+	public static void main(String[] args) {
+		System.out.println(Television.info);
+		System.out.println
+		(Television.from1To10Sum);
+
+	}
+
+}
