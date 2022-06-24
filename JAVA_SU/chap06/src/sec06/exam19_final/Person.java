f66370faef055de5563ad304d0c8f7db322eb5ee
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-25 02:28:22 +0900

    2022.06.25

---
+++ JAVA_SU/chap06/src/sec06/exam19_final/Person.java
@@ -0,0 +1,12 @@
+package sec06.exam19_final;
+
+public class Person {
+	final String nation ="korea";
+	final String ssn;
+	String name;
+	
+	public Person(String ssn, String name) {
+		this.ssn = ssn;
+		this.name = name;
+	}
+}
