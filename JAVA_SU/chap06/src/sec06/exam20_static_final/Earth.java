4cfd931862638ad5ef2dbdb676729847bcf34b62
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-28 02:07:20 +0900

    2022.06.28

---
+++ JAVA_SU/chap06/src/sec06/exam20_static_final/Earth.java
@@ -0,0 +1,14 @@
+package sec06.exam20_static_final;
+
+public class Earth {
+	static final double 
+	EARTH_RADIUS = 6400;
+	static final double
+	EARTH_SURFACE_AREA;
+	
+	static {
+		EARTH_SURFACE_AREA = 
+		4 * Math.PI * EARTH_RADIUS * 
+		EARTH_RADIUS;
+	}
+}
