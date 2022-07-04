be64b6180031a3c9372332d02dd8440ca802ad21
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-07-05 02:46:44 +0900

    2022.07.05

---
+++ JAVA_SU/chap06/src/sec06/exam25_field_method_class/package2/C.java
@@ -0,0 +1,18 @@
+package 
+sec06.exam25_field_method_class.package2;
+
+import 
+sec06.exam25_field_method_class.package1.*;
+
+public class C {
+	public C() {
+		A a = new A();
+		a.field1 = 1;
+		// a.field2 = 1;
+		// a.field3 = 1;
+		
+		a.method1();
+		// a.method2();
+		// a.method3();
+	}
+}
